fde111c6da03bea9cb2bd29e62ee902875632d80
weijin1990 <weijin_jp@qq.com>
2019-12-20 19:22:50 +0800

    Asset

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+//const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
+ // ,
+ // plugins: [
+ //   new ExtractTextPlugin("styles.css"),
+ // ]
 };
