a497db8c28741d2a50cdf020cc69af2f11f33a01
weijin1990 <weijin_jp@qq.com>
2019-12-21 14:49:34 +0800

    Output begin

--- webpack.config.js
+++ webpack.config.js
@@ -6,32 +6,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
   }
- // ,
- // plugins: [
- //   new ExtractTextPlugin("styles.css"),
- // ]
 };
